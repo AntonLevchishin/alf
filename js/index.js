0bc5f0da14873ed8f41453dc297d1081bd7a7272
AntonLevchishin <sharkiantik@gmail.com>
2020-04-28 16:54:48 +0300

    commit change

--- js/index.js
+++ js/index.js
@@ -1,46 +1,43 @@
 ﻿/* eslint-disable no-console */
 /* eslint-disable no-alert */
 
-import Router from './Router.js';
+import Router from "./Router.js";
+
+$.ajaxSetup({
+  // Disable caching of AJAX responses
+  cache: false,
+});
 
 const router = new Router({
-    mode: 'hash',
-    root: '/'
+  mode: "hash",
+  root: "/",
 });
 
 router
-    .add(/about/, () => {
-    alert('welcome in about page');
-    
-})
-.add(/news/, () => {
-    $( "#app" ).load( "pages/main.html" ,function (){
-        $( "#content" ).load( "pages/news.html",  function (){
-
-        });
-    })
-})
-.add(/products\/(.*)\/specification\/(.*)/, (id, specification) => {
-    alert(`products: ${id} specification: ${specification}`);
-    
-    console.log("111");
-})
-
-.add(/n\/(.*)/, (id) => {
-
-    $( "#app" ).load( "pages/main.html" ,function (){
-        $( "#content" ).load( "pages/news/"+ id + ".html",  function (){
-
-        });
+  .add(/about/, () => {
+    alert("welcome in about page");
+  })
+  .add(/news/, () => {
+    $("#app").load("pages/main.html", function () {
+      $("#content").load("pages/news.html", function () {});
     });
-})  
-.add('', () => {
+  })
+  .add(/products\/(.*)\/specification\/(.*)/, (id, specification) => {
+    alert(`products: ${id} specification: ${specification}`);
+
+    console.log("111");
+  })
+
+  .add(/n\/(.*)/, (id) => {
+    $("#app").load("pages/main.html", function () {
+      $("#content").load("pages/news/" + id + ".html", function () {});
+    });
+  })
+  .add("", () => {
     // general controller
     //console.log('welcome in catch all controller');
 
-    $( "#app" ).load( "pages/main.html" ,function (){
-        $( "#content" ).load( "pages/news.html",  function (){
-            
-        });
+    $("#app").load("pages/main.html", function () {
+      $("#content").load("pages/news.html", function () {});
     });
-});
+  });
